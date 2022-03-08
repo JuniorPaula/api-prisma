aa397c4091933ca2e98a0e26f9987bdca4a4571e
Junior Paula <slucivaldo509@gmail.com>
2022-03-08 17:10:51 -0300

    Create route from user profile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
@@ -15,5 +16,6 @@ router.post(
 );
 
 router.get('/messages/last3', new GetLast3MessagesController().handle);
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
